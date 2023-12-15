2fdcfcbc105ae395a346d37efc7d055a77100bb3
Naman Kumar Jaiswal <128512002+Naman-K-Jaiswal@users.noreply.github.com>
2023-12-16 02:48:00 +0530

    Committed the files
    GUI is working
    To be Done
      - Styling
      - Server Optimization -- I will do it
      - Mailing System

--- app_n.py
+++ app_n.py
@@ -6,7 +6,7 @@ import os
 from werkzeug.utils import secure_filename
 import csv
 from Cleaner1 import temp_files
-
+from Ash import main1
 UPLOAD_FOLDER = os.path.join('staticFiles', 'uploads')
 
 current_step = 1
@@ -28,16 +28,6 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'This is your secret key to utilize session in Flask'
 
 
-# def allowed_file(filename):
-# return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-# def save_file_and_session(file, session_key):
-#     data_filename = f'{session_key}.csv'
-#     file.save(os.path.join(app.config['UPLOAD_FOLDER'], data_filename))
-#     session[f'uploaded_data_file_path_{session_key}'] = os.path.join(app.config['UPLOAD_FOLDER'], data_filename)
-
-
 @app.route('/', methods=['GET', 'POST'])
 def uploadFile():
     global current_step
@@ -49,37 +39,46 @@ def uploadFile():
             current_step += 1
             f1 = request.files.get('f')
             if f1:
-                # data_filename1 = 'SCH.csv'
                 f1.save(os.path.join(app.config['UPLOAD_FOLDER'], 'SCH.csv'))
                 return render_template('index_new.html', rule_list=List1, default_values=default_values, default_checked=default_checked, current_step=current_step, file='PNR Passenger')
         elif current_step == 2:
             current_step += 1
             f2 = request.files.get('f')
             if f2:
-                # data_filename2 = 'PNRP.csv'
                 f2.save(os.path.join(app.config['UPLOAD_FOLDER'], 'PNRP.csv'))
                 return render_template('index_new.html', rule_list=List1, default_values=default_values, default_checked=default_checked, current_step=current_step, file='PNR Booking')
         elif current_step == 3:
             current_step += 1
             f3 = request.files.get('f')
             if f3:
-                # data_filename3 = 'PNRB.csv'
                 f3.save(os.path.join(app.config['UPLOAD_FOLDER'], 'PNRB.csv'))
                 return render_template('index_new.html', rule_list=List1, default_values=default_values, default_checked=default_checked, current_step=current_step, file='Inventory')
         elif current_step == 4:
             current_step += 1
             f4 = request.files.get('f')
             if f4:
-                # data_filename4 = 'INV.csv'
                 f4.save(os.path.join(app.config['UPLOAD_FOLDER'], 'INV.csv'))
+            return render_template('index_new_RuleEngine.html', rule_list=List1, default_values=default_values,                               default_checked=default_checked, current_step=current_step)
+
+        if current_step == 5:
+            rules = [['Condition', 'Score', 'FK']]
+            for i in range(1, 21):
+                rule_weight = int(request.form[f"rule{i}_weight"])
+                rule_bool = request.form.get(f"rule{i}_bool") is not None
+                rules.append([List1[i-1], rule_weight, rule_bool])
+
+            with open("staticFiles\\uploads\\RULES.csv", 'w', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerows(rules)
+
+            temp_files()
+
+            main1()
+
             return render_template('index_wtver.html')
 
-        # if current_step == 5:
-        #     return render_template('index_wtver.html')
-
         # If none of the above conditions match, show the current step
-        return render_template('index_new.html', rule_list=List1, default_values=default_values,
-                               default_checked=default_checked, current_step=current_step)
+        return render_template('index_new.html', rule_list=List1, default_values=default_values,                               default_checked=default_checked, current_step=current_step)
 
     return render_template('index_new.html', rule_list=List1, default_values=default_values,                        default_checked=default_checked, current_step=1, file='Flight Schedule')
 
